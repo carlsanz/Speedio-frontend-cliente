0d02ba32bc60cd5a6798e9a217738d62ff827f94
unknown <carlsanz_2013@yahoo.com>
2023-08-06 18:12:21 -0600

    creacion de productos individules

--- js/controlador.js
+++ js/controlador.js
@@ -1,18 +1,341 @@
+var restaurantes= [
+    {
+      "nombre": "Burger King ",
+      "direccion": "Calle 123, Ciudad X",
+      "telefono": "123-456-7890",
+      "tipo_comida": "Comida Rápida",
+      "horario": "Lunes a Viernes: 9:00 AM - 10:00 PM, Sábado y Domingo: 11:00 AM - 11:00 PM",
+      "imagen": "../img/burger-king.png",
+      "banner":"../img/banner-burger.png",
+      "menu": [
+        {
+          "nombre_plato": "Whopper",
+          "descripcion": "1/4 de libra de Res a la parrilla",
+          "precio": 201.00,
+          "img": "../img/whopper.png"
+        },
+        {
+          "nombre_plato": "Papas Fritas Grandes",
+          "descripcion": "Deliciosas y Crujientes papas fritas",
+          "precio": 61.00,
+          "img": "../img/papas-fritas.png"
+        },
+        {
+          "nombre_plato": "Refresco Grande",
+          "descripcion": "Refresco grande",
+          "precio": 50.00,
+          "img": "../img/refresco.png"
+        },
+        {
+          "nombre_plato": "Combo Para Tres",
+          "descripcion": "Incluye 1 king de pollo y dos Whopper Jr",
+          "precio": 355.00,
+          "img": "../img/combo-3.png"
+        },
+        {
+          "nombre_plato": "Combo Para dos",
+          "descripcion": "Incluye dos Whopper Jr",
+          "precio": 200.00,
+          "img": "../img/combo-2.png"
+        }
+      ]
+    },
+    {
+      "nombre": "Little Caesars",
+      "direccion": "Avenida 456, Ciudad Y",
+      "telefono": "987-654-3210",
+      "tipo_comida": "Comida Rápida",
+      "horario": "Todos los días: 11:30 AM - 9:00 PM",
+      "imagen": "../img/little-caesars.png",
+      "banner":"../img/banner-little.png",
+      "menu": [
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Clasica de pepperoni",
+          "descripcion": "Pizza Gigante",
+          "precio": 129.00,
+          "img": "../img/pizza-clasica.png"
+        },
+        {
+          "nombre_plato": "Super Cheese",
+          "descripcion": "Pizza Gigante",
+          "precio": 209.00,
+          "img": "../img/super-cheese.png"
+        },
+        {
+          "nombre_plato": "Pack Super Cheese",
+          "descripcion": "1 super cheese, 1 crazy bread, 1 refresco",
+          "precio": 309.00,
+          "img": "../img/pack-super.png"
+        },
+        {
+          "nombre_plato": "Refresco en Lata",
+          "descripcion": "Refresco en lata",
+          "precio": 33.00,
+          "img": "../img/lata.png"
+        }
+      ]
+    },
+    {
+      "nombre": "Coco Baleada",
+      "direccion": "Plaza 789, Ciudad Z",
+      "telefono": "456-123-7890",
+      "tipo_comida": "Comida China",
+      "horario": "Lunes a Sábado: 11:00 AM - 10:00 PM, Domingo: 12:00 PM - 9:00 PM",
+      "imagen": "../img/coco-baleadas.png",
+      "banner":"../img/banner-little.png",
+      "menu": [
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        }
+      ]
+    },
+    {
+      "nombre": "Wendys",
+      "direccion": "Calle Principal, Ciudad A",
+      "telefono": "111-222-3333",
+      "tipo_comida": "Comida Vegetariana",
+      "horario": "Lunes a Domingo: 11:00 AM - 9:00 PM",
+      "imagen": "../img/wendys.png",
+      "banner":"../img/banner-little.png",
+      "menu": [
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        }
+      ]
+    },
+    {
+      "nombre": "Churchs Chicken",
+      "direccion": "Avenida Principal, Ciudad B",
+      "telefono": "444-555-6666",
+      "tipo_comida": "Comida Japonesa",
+      "horario": "Lunes a Sábado: 12:00 PM - 10:00 PM",
+      "imagen": "../img/churchs.png",
+      "banner":"../img/banner-little.png",
+      "menu": [
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        }
+      ]
+    },
+    {
+      "nombre": "Bigos",
+      "direccion": "Plaza Central, Ciudad C",
+      "telefono": "777-888-9999",
+      "tipo_comida": "Comida India",
+      "horario": "Martes a Domingo: 11:30 AM - 10:00 PM",
+      "imagen": "../img/bigos.png",
+      "banner":"../img/banner-little.png",
+      "menu": [
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        },
+        {
+          "nombre_plato": "Dos Calzone",
+          "descripcion": "Dos calzone de un ingrediente",
+          "precio": 99.00,
+          "img": "../img/calzone.png"
+        }
+      ]
+    }
+  ]
+
+
+//ingreso a registrarse
 function landingPage() {
     document.getElementById("lading-page").style.display="none"
     document.getElementById("inicio-sesión").style.display="block"
+    document.getElementById("restaurantes").style.display="none"
+    document.getElementById("registrarte").style.display="none"
+    document.getElementById("restaurante-individual").style.display="none";
    
 
 }
 
-
+//ingreso a restaurantes
 function ingreso() {
+
+  document.getElementById("contenedor-restaurantes").innerHTML =""; 
     document.getElementById("lading-page").style.display="none"
     document.getElementById("inicio-sesión").style.display="none"
     document.getElementById("restaurantes").style.display="block"
+    document.getElementById("registrarte").style.display="none"
+    document.getElementById("restaurante-individual").style.display="none";
+
+   for (let i = 0; i < restaurantes.length; i++) {
+    document.getElementById("contenedor-restaurantes").innerHTML += 
+    `<div id="${restaurantes[i].nombre}" class="restaurante" onclick="restauranteIndividual(this)">
+      <img src=${restaurantes[i].imagen} alt="" >
+    </div>`
+    
+   }
+   document.getElementById("inicio-cambio").style.color="#C6382C";
+   
+  
+}
+
+
+
+//funcion Generar Registro
+function registro() {
+  document.getElementById("lading-page").style.display="none"
+  document.getElementById("inicio-sesión").style.display="none"
+  document.getElementById("restaurantes").style.display="none"
+  document.getElementById("registrarte").style.display="block"
+  document.getElementById("restaurante-individual").style.display="none";
+
+
+}
+
+
+function restauranteIndividual(element) {
+const indice = restaurantes.findIndex((restaurante) => restaurante.nombre === element.id);
+document.getElementById("contenedor-menus").innerHTML =""; 
+document.getElementById("contenedor-banner").innerHTML = "";
+
+  document.getElementById("lading-page").style.display="none";
+  document.getElementById("inicio-sesión").style.display="none";
+  document.getElementById("restaurantes").style.display="none";
+  document.getElementById("registrarte").style.display="none";
+  document.getElementById("restaurante-individual").style.display="block";
+
+  document.getElementById("contenedor-banner").innerHTML = 
+  `<img style="margin: 10px 10px 10px 10px; width: 100%;" src="${restaurantes[indice].banner}" alt="">`;
+
+ 
+
+  for (let i = 0; i< restaurantes[indice].menu.length; i++) {
+    document.getElementById("contenedor-menus").innerHTML += 
+    ` <div id="menu-individual">
+    <div id="contenedor-imagen-menu">
+       <img src="${restaurantes[indice].menu[i].img}" alt="">
+   </div>
+   <div id="contenedor-productos-individuales">
+       <p id="nombre-producto">${restaurantes[indice].menu[i].nombre_plato}</p>
+       <p id="descripcion-producto">${restaurantes[indice].menu[i].nombre_plato}</p>
+       <p id="precio-producto">Precio: L.${restaurantes[indice].menu[i].precio}</p>
+       <button id="boton-producto">Añadir al carrito</button>
+   </div> 
+</div>`;  
+
+      }
 
     
+
+
+  
+
+
 }
+
+
 
 
 
@@ -41,6 +364,7 @@ function ingreso() {
         }
 
       }
+
 
     
 
