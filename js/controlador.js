c5ade13443e755f394e9b96a284bf99aa9b60bae
unknown <carlsanz_2013@yahoo.com>
2023-08-24 23:58:22 -0600

    renderizando con información del backend para ingresar,los restaurantes y sus productos

--- js/controlador.js
+++ js/controlador.js
@@ -1,253 +1,48 @@
-var restaurantes= [
-    {
-      "nombre": "Burger King ",
-      "direccion": "Calle 123, Ciudad X",
-      "telefono": "123-456-7890",
-      "tipo_comida": "Comida Rápida",
-      "horario": "Lunes a Viernes: 9:00 AM - 10:00 PM, Sábado y Domingo: 11:00 AM - 11:00 PM",
-      "imagen": "../img/burger-king.png",
-      "banner":"../img/banner-burger.png",
-      "menu": [
-        {
-          "nombre_plato": "Whopper",
-          "descripcion": "1/4 de libra de Res a la parrilla",
-          "precio": 201.00,
-          "img": "../img/whopper.png"
-        },
-        {
-          "nombre_plato": "Papas Fritas Grandes",
-          "descripcion": "Deliciosas y Crujientes papas fritas",
-          "precio": 61.00,
-          "img": "../img/papas-fritas.png"
-        },
-        {
-          "nombre_plato": "Refresco Grande",
-          "descripcion": "Refresco grande",
-          "precio": 50.00,
-          "img": "../img/refresco.png"
-        },
-        {
-          "nombre_plato": "Combo Para Tres",
-          "descripcion": "Incluye 1 king de pollo y dos Whopper Jr",
-          "precio": 355.00,
-          "img": "../img/combo-3.png"
-        },
-        {
-          "nombre_plato": "Combo Para dos",
-          "descripcion": "Incluye dos Whopper Jr",
-          "precio": 200.00,
-          "img": "../img/combo-2.png"
-        }
-      ]
-    },
-    {
-      "nombre": "Little Caesars",
-      "direccion": "Avenida 456, Ciudad Y",
-      "telefono": "987-654-3210",
-      "tipo_comida": "Comida Rápida",
-      "horario": "Todos los días: 11:30 AM - 9:00 PM",
-      "imagen": "../img/little-caesars.png",
-      "banner":"../img/banner-little.png",
-      "menu": [
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Clasica de pepperoni",
-          "descripcion": "Pizza Gigante",
-          "precio": 129.00,
-          "img": "../img/pizza-clasica.png"
-        },
-        {
-          "nombre_plato": "Super Cheese",
-          "descripcion": "Pizza Gigante",
-          "precio": 209.00,
-          "img": "../img/super-cheese.png"
-        },
-        {
-          "nombre_plato": "Pack Super Cheese",
-          "descripcion": "1 super cheese, 1 crazy bread, 1 refresco",
-          "precio": 309.00,
-          "img": "../img/pack-super.png"
-        },
-        {
-          "nombre_plato": "Refresco en Lata",
-          "descripcion": "Refresco en lata",
-          "precio": 33.00,
-          "img": "../img/lata.png"
-        }
-      ]
-    },
-    {
-      "nombre": "Coco Baleada",
-      "direccion": "Plaza 789, Ciudad Z",
-      "telefono": "456-123-7890",
-      "tipo_comida": "Comida China",
-      "horario": "Lunes a Sábado: 11:00 AM - 10:00 PM, Domingo: 12:00 PM - 9:00 PM",
-      "imagen": "../img/coco-baleadas.png",
-      "banner":"../img/banner-little.png",
-      "menu": [
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        }
-      ]
-    },
-    {
-      "nombre": "Wendys",
-      "direccion": "Calle Principal, Ciudad A",
-      "telefono": "111-222-3333",
-      "tipo_comida": "Comida Vegetariana",
-      "horario": "Lunes a Domingo: 11:00 AM - 9:00 PM",
-      "imagen": "../img/wendys.png",
-      "banner":"../img/banner-little.png",
-      "menu": [
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        }
-      ]
-    },
-    {
-      "nombre": "Churchs Chicken",
-      "direccion": "Avenida Principal, Ciudad B",
-      "telefono": "444-555-6666",
-      "tipo_comida": "Comida Japonesa",
-      "horario": "Lunes a Sábado: 12:00 PM - 10:00 PM",
-      "imagen": "../img/churchs.png",
-      "banner":"../img/banner-little.png",
-      "menu": [
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        }
-      ]
-    },
-    {
-      "nombre": "Bigos",
-      "direccion": "Plaza Central, Ciudad C",
-      "telefono": "777-888-9999",
-      "tipo_comida": "Comida India",
-      "horario": "Martes a Domingo: 11:30 AM - 10:00 PM",
-      "imagen": "../img/bigos.png",
-      "banner":"../img/banner-little.png",
-      "menu": [
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        },
-        {
-          "nombre_plato": "Dos Calzone",
-          "descripcion": "Dos calzone de un ingrediente",
-          "precio": 99.00,
-          "img": "../img/calzone.png"
-        }
-      ]
-    }
-  ]
+
+  let restaurant=[];
+  let botonIngresar = document.getElementById("boton-inicio-sesion");
+
+  let usertxt = document.getElementById("email");
+  let passwordtxt = document.getElementById("contrasena");
+  let divProducto = document.getElementById("contenedor-productos-individuales");
+  let botonAgregarProducto = document.getElementById("boton-producto");
 
 
+
+botonIngresar.addEventListener("click", () => {
+  loginUser(usertxt.value, passwordtxt.value);
+});
+
+
+const loginUser = async (user, password) => {
+
+  let respuesta = await fetch(`http://localhost:3000/usuarios/login`, {
+      method: 'POST',
+      headers: {
+          
+          "Content-Type": "application/json", //MIME Type
+      },
+      body: JSON.stringify(
+          {
+              "correo_electronico": `${user}`,
+              "contrasena": `${password}`
+          }
+      )
+      
+  });
+  
+  let usuario = await respuesta.json();
+  
+  if(usuario.status==true){
+    ingreso();
+  }else {
+    validarFormulario();
+  }
+}
+// 
+
+
+// 
 //ingreso a registrarse
 function landingPage() {
     document.getElementById("lading-page").style.display="none"
@@ -255,12 +50,27 @@ function landingPage() {
     document.getElementById("restaurantes").style.display="none"
     document.getElementById("registrarte").style.display="none"
     document.getElementById("restaurante-individual").style.display="none";
+    document.getElementById("credit-card").style.display="none";
+    document.getElementById("direccion").style.display="none";
+    document.getElementById("pedidos").style.display= "none"
    
 
 }
 
+
+
 //ingreso a restaurantes
-function ingreso() {
+const ingreso = async()=> {
+
+  let respuesta = await fetch(`http://localhost:3000/restaurantes`, {
+      method: 'GET',
+      headers: {
+          "Content-Type": "application/json", //MIME Type
+      }
+  });
+  let restaurantes = await respuesta.json();
+  console.log(restaurantes);
+  restaurant=restaurantes;
 
   document.getElementById("contenedor-restaurantes").innerHTML =""; 
     document.getElementById("lading-page").style.display="none"
@@ -269,6 +79,9 @@ function ingreso() {
     document.getElementById("registrarte").style.display="none"
     document.getElementById("restaurante-individual").style.display="none";
     document.getElementById("carrito-compras").style.display="none";
+    document.getElementById("credit-card").style.display="none";
+    document.getElementById("direccion").style.display="none";
+    document.getElementById("pedidos").style.display= "none"
 
    for (let i = 0; i < restaurantes.length; i++) {
     document.getElementById("contenedor-restaurantes").innerHTML += 
@@ -285,7 +98,7 @@ function ingreso() {
 
 
 //funcion Generar Registro
-function registro() {
+const registro = async() => {
  
   document.getElementById("lading-page").style.display="none"
   document.getElementById("inicio-sesión").style.display="none"
@@ -293,13 +106,44 @@ function registro() {
   document.getElementById("registrarte").style.display="block"
   document.getElementById("restaurante-individual").style.display="none";
   document.getElementById("carrito-compras").style.display="none";
+  document.getElementById("credit-card").style.display="none";
+  document.getElementById("direccion").style.display="none";
+  document.getElementById("pedidos").style.display= "none"
+}
 
+const agregarNuevoUsuario= async ()=>{
+  
+  let respuesta = await fetch(`http://localhost:3000/usuarios/nuevo`, {
+      method: 'POST',
+      headers: {
+          
+          "Content-Type": "application/json", //MIME Type
+      },
+      body: JSON.stringify(
+          {
+            "nombre":`${document.getElementById("nombre-usuario").value}`,
+            "apellido":`${document.getElementById("apellido-usuario").value}`,
+            "telefono":`${document.getElementById("telefono-usuario").value}`,
+            "correo_electronico":`${document.getElementById("email-usuario").value}`,
+            "contrasena":`${document.getElementById("contrasena-usuario").value}`
+          }
+      )
+      
+      
+  });
+  
+  let usuario = await respuesta.json();
+  console.log(usuario);
+  landingPage();
 
 }
 
 
 function restauranteIndividual(element) {
-  const indice = restaurantes.findIndex((restaurante) => restaurante.nombre === element.id);
+
+  console.log(element.id);
+  const indice = restaurant.findIndex((restaurante) => restaurante.nombre === element.id);
+  console.log(indice);
     document.getElementById("contenedor-menus").innerHTML =""; 
     document.getElementById("contenedor-banner").innerHTML = "";
 
@@ -309,27 +153,61 @@ function restauranteIndividual(element) {
     document.getElementById("registrarte").style.display="none";
     document.getElementById("restaurante-individual").style.display="block";
     document.getElementById("carrito-compras").style.display="none";
+    document.getElementById("credit-card").style.display="none";
+    document.getElementById("direccion").style.display="none";
+    document.getElementById("pedidos").style.display= "none"
 
 
     document.getElementById("contenedor-banner").innerHTML = 
-      `<img style="margin: 10px 10px 10px 10px; width: 100%;" src="${restaurantes[indice].banner}" alt="">`;
+      `<img style="margin: 10px 10px 10px 10px; width: 100%;" src="${restaurant[indice].banner}" alt="">`;
 
-  for (let i = 0; i< restaurantes[indice].menu.length; i++) {
+  for (let i = 0; i< restaurant[indice].menu.length; i++) {
     document.getElementById("contenedor-menus").innerHTML += 
     ` <div id="menu-individual">
           <div id="contenedor-imagen-menu">
-            <img src="${restaurantes[indice].menu[i].img}" alt="">
+            <img src="${restaurant[indice].menu[i].img}" alt="">
           </div>
         <div id="contenedor-productos-individuales">
-          <p id="nombre-producto">${restaurantes[indice].menu[i].nombre_plato}</p>
-          <p id="descripcion-producto">${restaurantes[indice].menu[i].nombre_plato}</p>
-          <p id="precio-producto">Precio: L.${restaurantes[indice].menu[i].precio}</p>
+          <p id="nombre-producto">${restaurant[indice].menu[i].nombre_plato}</p>
+          <p id="descripcion-producto">${restaurant[indice].menu[i].nombre_plato}</p>
+          <p id="precio-producto">Precio: L.${restaurant[indice].menu[i].precio}</p>
           <button id="boton-producto">Añadir al carrito</button>
         </div> 
       </div>`;  
+
+     
   }
+  
 }
 
+// 
+// botonIngresar.addEventListener("click", () => {
+//   loginUser(usertxt.value, passwordtxt.value);
+// });
+
+// document.getElementById("contenedor-productos-individuales").addEventListener('click', () => {
+//   // Obtén todos los elementos dentro del div
+//   const elementos = divProducto.querySelectorAll('p');
+//   const valores = [];
+
+//   // Itera a través de los elementos y obtén su contenido
+//   elementos.forEach(elemento => {
+//       valores.push(elemento.textContent);
+//   });
+
+//   // Haz algo con los valores, por ejemplo, muestra en la consola
+//   console.log(valores);
+// });
+
+
+
+
+
+
+
+
+
+// 
 
 function carShopping(){
   
@@ -342,37 +220,55 @@ function carShopping(){
     document.getElementById("registrarte").style.display="none";
     document.getElementById("restaurante-individual").style.display="none";
     document.getElementById("carrito-compras").style.display="block";
+    document.getElementById("credit-card").style.display="none";
+    document.getElementById("direccion").style.display="none";
+    document.getElementById("pedidos").style.display= "none"
 
 }
 
-    function validarFormulario() {
-        const emailInput = document.getElementById('email');
-        const contrasenaInput = document.getElementById('contrasena');
-  
-        const email = emailInput.value;
-        const contrasena = contrasenaInput.value;
-  
-        // Expresión regular para validar el email
-        const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  
-        if (regexEmail.test(email)) {
-          
-        } else {
-          alert('Email inválido');
-          return;
-        }
-  
-        // Verificar que la contraseña tenga al menos 6 caracteres
-        if (contrasena.length < 6) {
-          alert('La contraseña debe tener al menos 6 caracteres');
-        } else {
-          ingreso()
-        }
+function agregarTarjeta () {
+   document.getElementById("lading-page").style.display="none";
+    document.getElementById("inicio-sesión").style.display="none";
+    document.getElementById("restaurantes").style.display="none";
+    document.getElementById("registrarte").style.display="none";
+    document.getElementById("restaurante-individual").style.display="none";
+    document.getElementById("carrito-compras").style.display="none";
+    document.getElementById("credit-card").style.display="block";
+    document.getElementById("direccion").style.display="none";
+    document.getElementById("pedidos").style.display= "none"
+}
 
+function agregarDireccion () {
+  document.getElementById("lading-page").style.display="none";
+   document.getElementById("inicio-sesión").style.display="none";
+   document.getElementById("restaurantes").style.display="none";
+   document.getElementById("registrarte").style.display="none";
+   document.getElementById("restaurante-individual").style.display="none";
+   document.getElementById("carrito-compras").style.display="none";
+   document.getElementById("credit-card").style.display="none";
+   document.getElementById("direccion").style.display="block";
+   document.getElementById("pedidos").style.display= "none"
+}
+
+function estadoPedido() {
+  document.getElementById("lading-page").style.display="none";
+   document.getElementById("inicio-sesión").style.display="none";
+   document.getElementById("restaurantes").style.display="none";
+   document.getElementById("registrarte").style.display="none";
+   document.getElementById("restaurante-individual").style.display="none";
+   document.getElementById("carrito-compras").style.display="none";
+   document.getElementById("credit-card").style.display="none";
+   document.getElementById("direccion").style.display="none";
+   document.getElementById("pedidos").style.display= "block"
+
+}
+
+
+
+    function validarFormulario() {
+      return alert('Email invalido o contraseña incorrecta');
     }
 
-
-    
 
 
 
